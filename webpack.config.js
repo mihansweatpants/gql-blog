29f6c69965a141b4c24136ba50e7f5b904abe773
brsgn <barsegyan.m@future-group.ru>
2018-12-29 20:55:16 +0300

    Fix identation

--- webpack.config.js
+++ webpack.config.js
@@ -6,38 +6,38 @@ const isDev = process.env.NODE_ENV === 'development';
 
 const plugins = [];
 if (isDev) {
-    plugins.push(new Dotenv());
+  plugins.push(new Dotenv());
 }
 
 module.exports = {
-    entry: ['./src/server.js'],
-    target: 'node',
-    watch: isDev,
-    externals: [nodeExternals()],
-    module: {
-        rules: [
-            {
-                test: /\.(graphql|gql)$/,
-                use: [
-                    {
-                        loader: 'graphql-tag/loader'
-                    }
-                ],
-                exclude: /node_modules/
-            }
-        ]
+  entry: ['./src/server.js'],
+  target: 'node',
+  watch: isDev,
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.(graphql|gql)$/,
+        use: [
+          {
+            loader: 'graphql-tag/loader',
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.mjs', '.js', '.json'],
+    alias: {
+      modules: path.resolve(__dirname, 'src/modules'),
+      helpers: path.resolve(__dirname, 'src/helpers/functions'),
+      '~': path.resolve(__dirname, 'src'),
     },
-    resolve: {
-        extensions: ['.mjs', '.js', '.json'],
-        alias: {
-            modules: path.resolve(__dirname, 'src/modules'),
-            helpers: path.resolve(__dirname, 'src/helpers/functions'),
-            '~': path.resolve(__dirname, 'src')
-        }
-    },
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    plugins,
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins,
 };
