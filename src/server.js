934824aa01cbef781293413792669a6c2bb2a7db
brsgn <barsegyan.m@future-group.ru>
2019-01-08 01:24:41 +0300

    Add CORS

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
+import cors from 'cors';
 import chalk from 'chalk';
 import cowsay from 'cowsay';
 
@@ -12,6 +13,7 @@ import oauth from '~/oauth';
 
 const app = express();
 
+app.use(cors());
 app.use('/oauth', oauth);
 
 const graphQLServer = new ApolloServer({
